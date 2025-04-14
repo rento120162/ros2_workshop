166d6bdf527332dc6cefecf55a5b9534e2c7704c
rento120162 <r120162@g.kobe-kosen.ac.jp>
2025-04-14 11:43:37 +0000

    add node that is control DCmotor2

--- src/Arduino/micro_ros_radiocontrol.ino
+++ src/Arduino/micro_ros_radiocontrol.ino
@@ -91,7 +91,7 @@ void setup() {
 
   //create init_options
   RCCHECK(rclc_support_init(&support, 0, NULL, &allocator));
-  // create node
+  // create node 
   RCCHECK(rclc_node_init_default(&node, "radio_control_node", "", &support));
   // create publisher
   RCCHECK(rclc_publisher_init_default(&publisher, &node, ROSIDL_GET_MSG_TYPE_SUPPORT(geometry_msgs, msg, Twist), "radio_control_pub"));
